723a8bf4b8da1e273fc70225012d6b70ae57142d
Berg72 <bergy450r@gmail.com>
2024-03-01 15:54:29 -0800

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Mark Bergeson on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
